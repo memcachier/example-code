8f41651f5280c6663fb21e617b96b353ab21f2d7
Sascha <sascha.trifunovic@gmail.com>
2018-04-02 13:51:12 +0200

    Add explicit config of memjs

--- node.js/memjs/example.js
+++ node.js/memjs/example.js
@@ -1,6 +1,19 @@
 var memjs = require('memjs')
 
-var mc = memjs.Client.create()
+var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+  failover: true,     // default: false
+  failoverTime: 30,   // default: 60 (seconds)
+  retries: 2,         // default: 2
+  retry_delay: 0.2,   // default: 0.2 (seconds)
+  expires: 0,         // default: 0 (never)
+  logger: console,    // default: console
+  timeout: 1.0,       // default: 0.5 (seconds)
+  conntimeout: 2.0,   // default: 2 * timeout
+  keepAlive: true,    // default: false
+  keepAliveDelay: 30, // default: 30 (seconds)
+  username: process.env.MEMCACHIER_USERNAME,
+  password: process.env.MEMCACHIER_PASSWORD
+})
 
 mc.set('hello', 'world', {expires:600}, function(err, val) {
   if(err != null) {
